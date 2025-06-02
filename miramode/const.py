6ec23b11742f1be6a99b7b49556d405d9bd391dd
Martin Grayson <martin@zeddy.org>
2025-06-02 21:56:47 +0100

    General refactor of existing lib

---
+++ miramode/const.py
@@ -0,0 +1,11 @@
+import miramode
+
+OUTLET_STATE_STR = {
+    miramode.OUTLET_STOPPED: "off",
+    miramode.OUTLET_RUNNING: "on",
+}
+TIMER_STATE_STR = {
+    miramode.TIMER_PAUSED: "paused",
+    miramode.TIMER_STOPPED: "stopped",
+    miramode.TIMER_RUNNING: "running",
+}
